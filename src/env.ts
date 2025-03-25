484ca5ce70e39b35373fabe1c768a57b2b3ed5c9
yuwol <yuwol036@gmail.com>
2025-03-25 14:29:44 +0900

    feat: Add Google authentication env variables

--- src/env.ts
+++ src/env.ts
@@ -3,5 +3,9 @@ import { z } from 'zod'
 export const env = z
   .object({
     DATABASE_URL: z.string(),
+    AUTH_GOOGLE_CLIENT_ID: z.string(),
+    AUTH_GOOGLE_CLIENT_SECRET: z.string(),
+    AUTH_GOOGLE_REDIRECT_URI: z.string().url(),
+    DEFAULT_REDIRECT_URL: z.string().url(),
   })
   .parse(process.env)
