31a632fd65f5e7020c3c01aadeb71e21266ca147
yuwol <yuwol036@gmail.com>
2025-04-08 13:40:15 +0900

    feat: Create index for primary email search

---
+++ src/db/migration/0003_opposite_songbird.sql
@@ -0,0 +1,1 @@
+CREATE INDEX `idx_email_01` ON `email` (`user_id`,`primary`);
--- src/db/schema/email.ts
+++ src/db/schema/email.ts
@@ -1,20 +1,30 @@
-import { boolean, int, mysqlTable, varchar } from 'drizzle-orm/mysql-core'
+import {
+  boolean,
+  index,
+  int,
+  mysqlTable,
+  varchar,
+} from 'drizzle-orm/mysql-core'
 import { createInsertSchema } from 'drizzle-zod'
 import { z } from 'zod'
 import { autoIncrementId, timestamps } from '@/db/fields'
 import { users } from '@/db/schema/user'
 
-export const emails = mysqlTable('email', {
-  id: autoIncrementId,
-  userId: int()
-    .notNull()
-    .references(() => users.id),
-  email: varchar({ length: 255 }).notNull().unique(),
-  verified: boolean().notNull().default(false),
-  primary: boolean().notNull().default(false),
-  createdAt: timestamps.createdAt,
-  updatedAt: timestamps.updatedAt,
-})
+export const emails = mysqlTable(
+  'email',
+  {
+    id: autoIncrementId,
+    userId: int()
+      .notNull()
+      .references(() => users.id),
+    email: varchar({ length: 255 }).notNull().unique(),
+    verified: boolean().notNull().default(false),
+    primary: boolean().notNull().default(false),
+    createdAt: timestamps.createdAt,
+    updatedAt: timestamps.updatedAt,
+  },
+  (table) => [index('idx_email_01').on(table.userId, table.primary)],
+)
 
 const emailInsertSchema = createInsertSchema(emails)
 
